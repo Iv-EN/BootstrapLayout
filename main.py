4d5aaa80405e70db0b68396044a389a688617e02
Iv-EN <engenivanov@yandex.ru>
2024-12-14 01:35:50 +0300

    completed an additional task

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MyServer(BaseHTTPRequestHandler):
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
-    def do_GET(self):
+    def do_GET(self) -> None:
         """ Метод для обработки входящих GET-запросов """
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -16,6 +16,18 @@ class MyServer(BaseHTTPRequestHandler):
         with open('templates/contacts.html', encoding='utf-8') as file:
             html_content = file.read()
         self.wfile.write(bytes(html_content, "utf-8"))
+
+    def do_POST(self) -> None:
+        """ Метод для обработки входящих POST-запросов """
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        response = post_data.decode('utf-8')
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        print(f'Получено от пользователя: {response}')
+
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
